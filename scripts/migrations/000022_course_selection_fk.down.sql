7ef3e90c6295e887029e8e5793fdb0342ea3f59c
besasch88 <lorenzo@castellimail.it>
2026-04-17 23:54:38 +0200

    fix: update course selection FK cascade on Menu Item

---
+++ scripts/migrations/000022_course_selection_fk.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "hueat_course_selection" DROP CONSTRAINT "fk_hueat_course_selection_menu_item";
+
+ALTER TABLE "hueat_course_selection"
+ADD CONSTRAINT "fk_hueat_course_selection_menu_item"
+FOREIGN KEY ("menu_item_id")
+REFERENCES "hueat_menu_item" ("id")
+ON DELETE SET NULL
+ON UPDATE CASCADE;
